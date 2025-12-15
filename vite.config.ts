abf521bc6af5b3049b3fb392a838b44845dd0ae3
unknown <anityasinaga331@gmail.com>
2025-12-15 10:31:39 +0700

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base: "/Galeri-Rupakarya-Balikpapan-/",
+  plugins: [vue()]
 })
